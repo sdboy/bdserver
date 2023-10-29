69d73c503330e7a430c2067016fce1dcef201a82
sdboy <jiaoyiguanggo@gmial.com>
2023-10-29 17:30:41 +0800

    init

---
+++ downlink-service/src/test/java/net/eyeb/bdserver/downlink/DownlinkServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.eyeb.bdserver.downlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DownlinkServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
