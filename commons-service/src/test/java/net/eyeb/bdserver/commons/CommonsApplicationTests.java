77bdc921e077f2d1f0fe42683842c93924c28582
sdboy <jiaoyiguanggo@gmial.com>
2023-10-29 17:46:31 +0800

    init

---
+++ commons-service/src/test/java/net/eyeb/bdserver/commons/CommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.eyeb.bdserver.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
---
+++ downlink-service/src/main/java/net/eyeb/bdserver/downlink/DownlinkServiceApplication.java
@@ -0,0 +1,13 @@
+package net.eyeb.bdserver.downlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DownlinkServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DownlinkServiceApplication.class, args);
+	}
+
+}
